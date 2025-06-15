06e0d363d2900fd306ebe15bdd6c8041db666e91
MihirPrajapati04 <prajapatimihir685@gmail.com>
2025-06-15 15:36:38 +0530

    fixed prerender issue of navbar by wrapping navbar inside the suspense

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Navbar } from "@/components/home/header/navbar";
 import { AuthLoader } from "@/components/home/auth-loader";
+import { Suspense } from "react";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       {/* This is a separate server component where Prisma can be used */}
       <AuthLoader />
-      <Navbar />
+      <Suspense fallback={<div>Loading navbar...</div>}>
+        <Navbar />
+      </Suspense>
       {children}
     </div>
   );
